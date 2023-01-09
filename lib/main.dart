b54dee64f221bdf6c4e4fffd6c4021810ca25c0f
souvik150 <souvikmukherjee150@gmail.com>
2023-01-10 03:38:27 +0530

    feat: adds counter

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,92 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_cubit_bloc_tutorial/pages/weather_search_page.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_cubit_bloc_tutorial/cubit/counter_cubit.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Material App',
-      home: WeatherSearchPage(),
+    return new BlocProvider<CounterCubit>(
+      create: (context) => CounterCubit(),
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: new ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: new MyHomePage(title: 'Flutter Hello World'),
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => new _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text(widget.title),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text("You have pushed this button this many times"),
+          BlocBuilder<CounterCubit, CounterState>(
+            builder: (context, state) {
+              return Text(
+                state.counterValue.toString(),
+                style: Theme.of(context).textTheme.headline4,
+              );
+            },
+          ),
+          SizedBox(
+            height: 24,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              FloatingActionButton(
+                onPressed: (() {
+                  BlocProvider.of<CounterCubit>(context).decrement();
+                }),
+                tooltip: 'Decrement',
+                child: Icon(Icons.remove),
+              ),
+              FloatingActionButton(
+                onPressed: (() {
+                  BlocProvider.of<CounterCubit>(context).increment();
+                }),
+                tooltip: 'Increment',
+                child: Icon(Icons.add),
+              ),
+            ],
+          )
+        ],
+      ),
     );
   }
 }
